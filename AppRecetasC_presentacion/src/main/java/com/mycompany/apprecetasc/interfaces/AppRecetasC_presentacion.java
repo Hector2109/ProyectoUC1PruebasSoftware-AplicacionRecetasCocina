78792bcd5261591ccc862faa87b4b7fdfd437118
JesusGCastro <105814927+JesusGCastro@users.noreply.github.com>
2024-09-04 11:06:05 -0700

    Creacion de proyecto interfaces
    Se hizo el proyecto e interfaces

---
+++ AppRecetasC_presentacion/src/main/java/com/mycompany/apprecetasc/interfaces/AppRecetasC_presentacion.java
@@ -0,0 +1,9 @@
+package com.mycompany.apprecetasc.interfaces;
+
+
+public class AppRecetasC_presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
