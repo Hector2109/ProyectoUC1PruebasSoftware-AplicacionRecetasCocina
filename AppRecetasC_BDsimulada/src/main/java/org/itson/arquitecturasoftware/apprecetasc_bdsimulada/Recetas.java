cad09f3c4a7630134706c556a281cd5a8e4010f1
Enrique Rodríguez Angulo <enrique.rodriguez246966@potros.itson.edu.mx>
2024-09-09 20:53:05 -0700

    Base de datos en receta finished

--- AppRecetasC_BDsimulada/src/main/java/org/itson/arquitecturasoftware/apprecetasc_bdsimulada/Recetas.java
+++ AppRecetasC_BDsimulada/src/main/java/org/itson/arquitecturasoftware/apprecetasc_bdsimulada/Recetas.java
@@ -45,6 +45,7 @@ public class Recetas {
         
         this.listaRecetas.add(new Receta("Pizza Margarita", 30, "Italiana", listaPasosPizza, listaIngredientesPizza));
         
+        
         //Guacamole a la bisu
             //Lista de pasos
         LinkedList <Paso> listaPasosGuacamole = new LinkedList<>();
@@ -55,7 +56,7 @@ public class Recetas {
         
             //Lista ingredientes
         LinkedList <Ingrediente> listaIngredientesGuacamole = new LinkedList<>();
-        listaIngredientesGuacamole.add(new Ingrediente("Aguacate", 2, "´piezas"));
+        listaIngredientesGuacamole.add(new Ingrediente("Aguacate", 2, "piezas"));
         listaIngredientesGuacamole.add(new Ingrediente("Tomate", 1, "piezas"));
         listaIngredientesGuacamole.add(new Ingrediente("Cebolla", 1, "piezas"));
         listaIngredientesGuacamole.add(new Ingrediente("Jugo de limón", 1, "piezas"));
@@ -64,42 +65,88 @@ public class Recetas {
         listaIngredientesGuacamole.add(new Ingrediente("Pimienta", 10, "gramos"));
         this.listaRecetas.add(new Receta("Guacamole a la bisu", 10, "Mexicana", listaPasosGuacamole, listaIngredientesGuacamole));
         
+        
         //Pasta Toro.rar
             //Lista de pasos
         LinkedList <Paso> listaPasosPasta = new LinkedList<>();
-        listaPasosPasta.add(new Paso(1, "Cocina "));
-        listaPasosPasta.add(new Paso(2, "Añade tomate y cebolla picados."));
-        listaPasosPasta.add(new Paso(3, "Agrega jugo de limón, cilantro, sal y pimienta al gusto."));
+        listaPasosPasta.add(new Paso(1, "Cocina la pasta según las instrucciones."));
+        listaPasosPasta.add(new Paso(2, "En una sartén, saltea el ajo en aceite de oliva hasta dorar."));
+        listaPasosPasta.add(new Paso(3, "Añade la pasta cocida, perejil, sal y pimienta."));
         listaPasosPasta.add(new Paso(4, "Mezcla bien y sirve."));
         
             //Lista ingredientes
         LinkedList <Ingrediente> listaIngredientesPasta = new LinkedList<>();
-        listaIngredientesPasta.add(new Ingrediente("Aguacate", 2, "´piezas"));
-        listaIngredientesPasta.add(new Ingrediente("Tomate", 1, "piezas"));
-        listaIngredientesPasta.add(new Ingrediente("Cebolla", 1, "piezas"));
-        listaIngredientesPasta.add(new Ingrediente("Jugo de limón", 1, "piezas"));
-        listaIngredientesPasta.add(new Ingrediente("Cilantro picado", 10, "mililitros"));
+        listaIngredientesPasta.add(new Ingrediente("Pasta", 200, "gramos"));
+        listaIngredientesPasta.add(new Ingrediente("Dientes de ajo", 3, "piezas"));
+        listaIngredientesPasta.add(new Ingrediente("Aceite de oliva", 10, "mililitro"));
+        listaIngredientesPasta.add(new Ingrediente("Perejil picado", 1, "piezas"));
         listaIngredientesPasta.add(new Ingrediente("Sal", 10, "gramos"));
         listaIngredientesPasta.add(new Ingrediente("Pimienta", 10, "gramos"));
-        this.listaRecetas.add(new Receta("Pasta Toro.rar", 30, "Italiana", listaPasosPasta, null));
+        this.listaRecetas.add(new Receta("Pasta Toro.rar", 30, "Milo Origins", listaPasosPasta, listaIngredientesPasta));
         
-        //
-        this.listaRecetas.add(new Receta("Pizza", 30, "Italiana", null, null));
         
-        //
-        this.listaRecetas.add(new Receta("Pizza", 30, "Italiana", null, null));
+        //Ensalada Amy
+            //Lista de pasos
+        LinkedList <Paso> listaPasosEnsalada = new LinkedList<>();
+        listaPasosEnsalada.add(new Paso(1, "Corta los tomates y la mozzarella en rodajas."));
+        listaPasosEnsalada.add(new Paso(2, "Colócalos alternados en un plato."));
+        listaPasosEnsalada.add(new Paso(3, "Añade albahaca, aceite de oliva, sal y pimienta."));
+        listaPasosEnsalada.add(new Paso(4, "Sirve fresca."));
         
-        //
-        this.listaRecetas.add(new Receta("Pizza", 30, "Italiana", null, null));
+            //Lista ingredientes
+        LinkedList <Ingrediente> listaIngredientesEnsalada = new LinkedList<>();
+        listaIngredientesEnsalada.add(new Ingrediente("Tomates", 2, "piezas"));
+        listaIngredientesEnsalada.add(new Ingrediente("Mozzarella", 200, "gramos"));
+        listaIngredientesEnsalada.add(new Ingrediente("Hojas de albahaca", 3, "piezas"));
+        listaIngredientesEnsalada.add(new Ingrediente("Aceite de oliva", 10, "mililitros"));
+        listaIngredientesEnsalada.add(new Ingrediente("Sal", 10, "gramos"));
+        listaIngredientesEnsalada.add(new Ingrediente("Pimienta", 10, "gramos"));
+        this.listaRecetas.add(new Receta("Pizza", 30, "Guasave core", listaPasosEnsalada, listaIngredientesEnsalada));
         
-        //
-        this.listaRecetas.add(new Receta("Pizza", 30, "Italiana", null, null));
+        
+        //Tostadas de Aguacate a la touna
+            //Lista de pasos
+        LinkedList <Paso> listaPasosTostada = new LinkedList<>();
+        listaPasosTostada.add(new Paso(1, "Tosta el pan."));
+        listaPasosTostada.add(new Paso(2, "Aplasta el aguacate y esparce sobre el pan."));
+        listaPasosTostada.add(new Paso(3, "Cocina un huevo a tu gusto y colócalo encima."));
+        listaPasosTostada.add(new Paso(4, "Añade sal, pimienta y un chorrito de limón."));
+        
+            //Lista ingredientes
+        LinkedList <Ingrediente> listaIngredientesTostada = new LinkedList<>();
+        listaIngredientesTostada.add(new Ingrediente("Rebanada de pan", 2, "piezas"));
+        listaIngredientesTostada.add(new Ingrediente("Aguacate", 3, "piezas"));
+        listaIngredientesTostada.add(new Ingrediente("Huevo", 10, "piezas"));
+        listaIngredientesTostada.add(new Ingrediente("Limón", 10, "piezas"));
+        listaIngredientesTostada.add(new Ingrediente("Sal", 10, "gramos"));
+        listaIngredientesTostada.add(new Ingrediente("Pimienta", 10, "gramos"));
+        this.listaRecetas.add(new Receta("Tostadas de Aguacate a la touna", 30, "Italiana", listaPasosTostada, listaIngredientesTostada));
+        
+        
+        //Pollo a la domitsu
+            //Lista de pasos
+        LinkedList <Paso> listaPasosPollo = new LinkedList<>();
+        listaPasosPollo.add(new Paso(1, "Cocina las pechugas en cubos en una sartén con aceite."));
+        listaPasosPollo.add(new Paso(2, "Añade la cebolla picada y sofríe."));
+        listaPasosPollo.add(new Paso(3, "Agrega el curry y la leche de coco."));
+        listaPasosPollo.add(new Paso(4, "Cocina a fuego bajo por 10 minutos. Sirve con arroz."));
+        
+            //Lista ingredientes
+        LinkedList <Ingrediente> listaIngredientesPollo = new LinkedList<>();
+        listaIngredientesPollo.add(new Ingrediente("Pechuga de pollo", 2, "piezas"));
+        listaIngredientesPollo.add(new Ingrediente("Cebolla", 1, "piezas"));
+        listaIngredientesPollo.add(new Ingrediente("Leche de coco", 200, "mililitros"));
+        listaIngredientesPollo.add(new Ingrediente("Curry en polvo", 2, "cucharadas"));
+        listaIngredientesPollo.add(new Ingrediente("Sal", 10, "gramos"));
+        listaIngredientesPollo.add(new Ingrediente("Pimienta", 10, "gramos"));
+        this.listaRecetas.add(new Receta("Pizza", 30, "Italiana", listaPasosPollo, listaIngredientesPollo));
+        
         
         
     }
 
     
-    public LinkedList<Receta> getUsuarios(){
+    public LinkedList<Receta> getRecetas(){
         return listaRecetas;
     }
     
