67bb0ca0046977b7d0810ff377ba419ac8119021
Enrique Rodríguez Angulo <enrique.rodriguez246966@potros.itson.edu.mx>
2024-09-09 20:09:54 -0700

    Primer avance de la BDA receta

--- AppRecetasC_BDsimulada/src/main/java/org/itson/arquitecturasoftware/apprecetasc_bdsimulada/Recetas.java
+++ AppRecetasC_BDsimulada/src/main/java/org/itson/arquitecturasoftware/apprecetasc_bdsimulada/Recetas.java
@@ -1,5 +1,9 @@
 package org.itson.arquitecturasoftware.apprecetasc_bdsimulada;
 
+import java.util.LinkedList;
+import org.itson.arquitecturasoftware.apprecetasc_entidad.Receta;
+
+
 /**
  *
  * @author Hector Espinoza
@@ -8,5 +12,28 @@ package org.itson.arquitecturasoftware.apprecetasc_bdsimulada;
  * @author Victoria Vega
  */
 public class Recetas {
+        
+    
+    LinkedList listaRecetas;
+    
+    /**
+     * Constructor para simular usuarios en BD
+     */
+    public Recetas(){ 
+        LinkedList <Receta> listaRecetas = new LinkedList<>();    
+        this.listaRecetas = listaRecetas;
+        
+        this.listaRecetas.add(new Receta("Pizza", 30, "Italiana", null));
+        
+        
+    }
+    
+    public LinkedList<Receta> getUsuarios(){
+        return listaRecetas;
+    }
+    
+    
+    
+    
     
 }
