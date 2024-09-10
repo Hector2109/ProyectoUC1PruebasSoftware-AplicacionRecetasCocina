826aaf5a071990a1d583829dbf7c0d2186c74f9c
Hector2109 <hector.espinoza247571@potros.itson.edu.mx>
2024-09-09 19:59:45 -0700

    Base de datos simulada

---
+++ AppRecetasC_BDsimulada/src/main/java/org/itson/arquitecturasoftware/apprecetasc_bdsimulada/Recetas.java
@@ -0,0 +1,12 @@
+package org.itson.arquitecturasoftware.apprecetasc_bdsimulada;
+
+/**
+ *
+ * @author Hector Espinoza
+ * @author Jesus Castro
+ * @author Enrique Rodriguez
+ * @author Victoria Vega
+ */
+public class Recetas {
+    
+}
